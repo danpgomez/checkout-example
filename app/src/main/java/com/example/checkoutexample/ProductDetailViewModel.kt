4af8c5770e3529fb99121d632be632afab7e98ea
danpgomez <dapgo3@gmail.com>
2023-01-27 17:49:08 -0500

    ViewModel converted to LiveData

--- app/src/main/java/com/example/checkoutexample/ProductDetailViewModel.kt
+++ app/src/main/java/com/example/checkoutexample/ProductDetailViewModel.kt
@@ -1,28 +1,38 @@
 package com.example.checkoutexample
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ProductDetailViewModel : ViewModel() {
     private val price = 5
-    var remainingStock = 5
-    var orderTotal = 0
-    var insufficientStock = false
+
+    private val _remainingStock = MutableLiveData(5)
+    val remainingStock: LiveData<Int> get() = _remainingStock
+
+    private val _orderTotal = MutableLiveData(0)
+    val orderTotal: LiveData<Int> get() = _orderTotal
+
+    private val _insufficientStock = MutableLiveData(false)
+    val insufficientStock: LiveData<Boolean> get() = _insufficientStock
 
     fun onAddButtonClicked(quantity: Int) {
-        insufficientStock = if (quantity <= remainingStock) {
-            updateStock(quantity)
-            updateOrderTotal(quantity)
-            false
-        } else {
-            true
+        _remainingStock.value?.let {
+            _insufficientStock.value = if (quantity <= it) {
+                updateStock(quantity)
+                updateOrderTotal(quantity)
+                false
+            } else {
+                true
+            }
         }
     }
 
     private fun updateOrderTotal(quantity: Int) {
-        orderTotal += quantity * price
+        _orderTotal.value = _orderTotal.value?.plus(quantity * price)
     }
 
     private fun updateStock(quantity: Int) {
-        remainingStock -= quantity
+        _remainingStock.value = _remainingStock.value?.minus(quantity)
     }
 }
